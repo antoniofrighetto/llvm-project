472f26de56100282959ff6dd9068c45f88c9243d
Antonio Frighetto <me@antoniofrighetto.com>
2024-07-06 15:51:33 +0200

    tmp

--- llvm/lib/Passes/PassBuilderPipelines.cpp
+++ llvm/lib/Passes/PassBuilderPipelines.cpp
@@ -1486,6 +1486,9 @@ PassBuilder::buildModuleOptimizationPipeline(OptimizationLevel Level,
 
   addVectorPasses(Level, OptimizePM, /* IsFullLTO */ false);
 
+  //TODO[antonio]
+  OptimizePM.addPass(BDCEPass());
+
   // LoopSink pass sinks instructions hoisted by LICM, which serves as a
   // canonicalization pass that enables other optimizations. As a result,
   // LoopSink pass needs to be a very late IR pass to avoid undoing LICM
